146534e8ec469e86de09c113e22eaed5ebfdedb6
Tianle Yu <yulele28@126.com>
2024-04-16 17:33:31 -0700

    [feat]first compiling version of Location service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -6,6 +6,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+import java.util.List;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
 
 @Service
 public class LocationQueryService {
@@ -20,6 +32,16 @@ public class LocationQueryService {
 
     public String getJSON(String location) throws HttpClientErrorException {
         Map<String, String> uriVariables = Map.of("location", location);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.TEXT_PLAIN));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class, uriVariables);
+        String csvData =  re.getBody();
+        // List<CollegeSubreddit> subreddits = new CsvToBeanBuilder<CollegeSubreddit>(new StringReader(csvData)).withType(CollegeSubreddit.class).build().parse();
+        // String jsonData = mapper.writeValueAsString(subreddits);
         return "";
     }
 }
